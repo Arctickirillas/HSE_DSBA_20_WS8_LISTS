5414bcfef6f6572d5524f931f6187e101444ff4a
Ivan Travnikov <39193727+Travonka@users.noreply.github.com>
2020-10-01 21:24:40 +0300

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,14 @@
+import math
+ar = input().split()
+base = []
+answer = [base]
+
+for i in range(len(ar)): 
+    original = answer[:]
+    new=ar[i]
+    for j in range(len(answer)):
+        answer[j] = answer[j]+ [new]
+    answer = original + answer
+print(answer)
+
+    
