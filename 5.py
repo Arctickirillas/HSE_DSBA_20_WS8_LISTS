82663f290b51c3124fb0cbb4bcb489c624548f19
Anton <48492872+YourStyle@users.noreply.github.com>
2020-10-01 22:42:35 +0300

    Add files via upload

--- 5.py
+++ 5.py
@@ -1,10 +1,9 @@
-string =  input().split()
-base = []   
-lists = [base] 
+string =  input().split() 
+lists = [[]] 
 for i in range(len(string)): 
     orig = lists[:]
-    new = string[i] 
+    new_list = string[i] 
     for j in range(len(lists)): 
-        lists[j] = lists[j] + [new] 
+        lists[j] = lists[j] + [new_list] 
     lists = orig + lists
 print(lists)
